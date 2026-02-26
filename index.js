f9fc51c85c94b26ee28831c84b0071df90bbe579
margitviigi <margit.viigi@voco.ee>
2026-02-26 10:53:33 +0200

    Install basic packages and create connection to database

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const sequelize = require('./util/db');
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+app.get('/', (req, res) => {
+  res.send( {message: 'web shop app'});
+});
+
+app.listen(3002, () => {
+  console.log('Server is running on port 3002');
+});
